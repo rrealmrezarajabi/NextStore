b890dc8ab4ba775450607984ddc5dd06e2e211e7
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-02-12 14:09:16 +0330

    limited categories to 8

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -27,7 +27,7 @@ export default async function ProductsPage({
   return (
     <main className="min-h-dvh bg-zinc-950 text-white">
       <div className="mx-auto max-w-6xl px-4 py-10">
-        {/* Search + Title */}
+        
         <div className="mb-8">
           <SearchBar />
         </div>
@@ -44,7 +44,7 @@ export default async function ProductsPage({
 
           <div className="w-full lg:w-64">
             <CategoriesSidebar
-              categories={categories}
+              categories={categories.slice(0,8)}
               activeCategoryId={categoryId}
             />
           </div>
