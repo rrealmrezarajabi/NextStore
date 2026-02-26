93b0b11f6312519196f4af5131579bf6346d324b
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-02-26 16:11:28 +0330

    auth user type added

---
+++ lib/auth/auth-context.tsx
@@ -0,0 +1,4 @@
+"use client"
+
+import { createContext, useContext, useState, ReactNode } from "react";
+
---
+++ types/auth.ts
@@ -0,0 +1,9 @@
+export interface AuthUser {
+  id: number;
+  email: string;
+  role: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  avatar: string;
+}
