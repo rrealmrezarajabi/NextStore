12b28cde1d9cf27365c7c4ae12118d8ed589acea
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-02-26 16:51:11 +0330

    added provider to root layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "@/providers";
+import { getSession } from "@/lib/auth/get-session";
 
 export const metadata: Metadata = {
   title: "NextStore",
-  description: "A modern ecommerece website",
+  description: "A modern ecommerce website",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getSession();
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers initialUser={user}>{children}</Providers>
+      </body>
     </html>
   );
 }
