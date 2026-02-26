33819597013daeb749d7bf0a931ff93cb68eae32
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-02-26 16:49:56 +0330

    provider added

--- providers.tsx
+++ providers.tsx
@@ -2,11 +2,21 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { AuthProvider } from "@/lib/auth/auth-context";
+import { AuthUser } from "./types/auth";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({
+  children,
+  initialUser,
+}: {
+  children: React.ReactNode;
+  initialUser: AuthUser | null;
+}) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider initialUser={initialUser}>{children}</AuthProvider>
+    </QueryClientProvider>
   );
 }
