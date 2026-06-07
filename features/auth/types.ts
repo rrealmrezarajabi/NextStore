b6a9a9e6a55c0db7841197ab1f0dac16570d79f4
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-06-07 15:44:38 +0330

    register and login page with their schemas and types created

---
+++ features/auth/types.ts
@@ -0,0 +1,7 @@
+import { registerSchema } from "./schemas/register.schema";
+import { loginSchema } from "./schemas/login.schema";
+import z from "zod";
+
+
+export type LoginDTO = z.infer<typeof loginSchema>
+export type RegisterDTO = z.infer<typeof registerSchema>
