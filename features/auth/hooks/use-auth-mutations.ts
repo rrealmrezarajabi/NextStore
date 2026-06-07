c3e19d3783eff5c2776133c5c9ff547c9e32ec02
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-06-07 15:55:25 +0330

    auth hooks for mutation added

---
+++ features/auth/hooks/use-auth-mutations.ts
@@ -0,0 +1,45 @@
+"use client";
+
+import { profileQueryKeys } from "@/features/profile/hooks/use-profile-queries";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { login, logout, register } from "../services/auth.service";
+
+export function useLogin() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: login,
+    onSuccess: ({ user }) => {
+      queryClient.setQueryData(profileQueryKeys.all, user);
+      router.push("/dashboard");
+    },
+  });
+}
+
+export function useRegister() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: register,
+    onSuccess: ({ user }) => {
+      queryClient.setQueryData(profileQueryKeys.all, user);
+      router.push("/dashboard");
+    },
+  });
+}
+
+export function useLogout() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: profileQueryKeys.all });
+      router.push("/login");
+    },
+  });
+}
--- features/auth/types.ts
+++ features/auth/types.ts
@@ -1,7 +1,6 @@
 import { registerSchema } from "./schemas/register.schema";
 import { loginSchema } from "./schemas/login.schema";
-import z from "zod";
+import { z } from "zod";
 
-
-export type LoginDTO = z.infer<typeof loginSchema>
-export type RegisterDTO = z.infer<typeof registerSchema>
+export type LoginDTO = z.infer<typeof loginSchema>;
+export type RegisterDTO = z.infer<typeof registerSchema>;
